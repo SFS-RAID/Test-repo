36e6cfb3c41ea76df52ce973243a15386c05437f
SFSRAID <99122481+SFS-RAID@users.noreply.github.com>
2023-01-04 01:29:14 +0000

    Add names

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 uin = input("Type the amount of money:")
+name = input("Type your name: ")
 
 with open('mydata.txt', 'w') as file:
-  file.writelines(['Money = ' + uin + '\n', 'name = me'])
+  file.writelines(['Money = ' + uin + '\n', 'name = ' + name])
 with open('mydata.txt', 'r') as file:
   contents = file.readlines()
   moneyline = contents[0].split(" ")
   money = moneyline[-1]
+  nameline = contents[1].split(" ")
+  name = nameline[-1]
 print(f"The amount of money is {money}")
+print(f'Your name is {name}')
